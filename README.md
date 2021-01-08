79e28b374e2d53e286d507e7d7c8bfcb7317426e
lalinnx <lalinpye@gmail.com>
2021-01-08 16:26:55 +0700

    finish

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:lalinthorn pholnaruk
+# STUDENT ID:630610758
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,23 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+	double mean=0,sd=0,num=0;
+	ifstream score;
+    string textline;
+    score.open("score.txt");
+    while (getline(score,textline)){	
+        mean += stod(textline);
+		sd += stod(textline)*stod(textline);
+		num++;
+	}
+	
+    cout << "Number of data = "<< num << "\n";
+    cout << "Mean = "<< setprecision(3) << mean/num << "\n";
+    cout << "Standard deviation = "<< sqrt((sd/num)-(pow((mean/num),2)));
+ 
+}
